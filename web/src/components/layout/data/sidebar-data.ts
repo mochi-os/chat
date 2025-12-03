770a496be4d5f2f467a96fac9a0f057beebe5daa
Numan <numan.saleem08@gmail.com>
2025-12-03 14:31:28 +0000

    add feeds route

--- web/src/components/layout/data/sidebar-data.ts
+++ web/src/components/layout/data/sidebar-data.ts
@@ -3,9 +3,10 @@ import {
   Home,
   LayoutTemplate,
   MessagesSquare,
+  Newspaper,
   UserPlus,
 } from 'lucide-react'
-import { APP_ROUTES } from '@/config/routes'
+import { APP_ROUTES } from '@/config/app-routes'
 import { type SidebarData } from '../types'
 
 export const sidebarData: SidebarData = {
@@ -34,6 +35,12 @@ export const sidebarData: SidebarData = {
           title: 'Notifications',
           url: APP_ROUTES.NOTIFICATIONS.HOME,
           icon: Bell,
+          external: true,
+        },
+        {
+          title: 'Feeds',
+          url: APP_ROUTES.FEEDS.HOME,
+          icon: Newspaper,
           external: true,
         },
         {
