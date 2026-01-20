99c1099f74aae251f150fe02383c5e01eaf40f5b
Alistair Cunningham <acunningham@integrics.com>
2026-01-21 10:10:51 +1300

    Use mochiPlugin to prevent dark mode flash

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { mochiPlugin } from '@mochi/common/vite'
 
 export default defineConfig({
   base: './',
   plugins: [
+    mochiPlugin(),
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
